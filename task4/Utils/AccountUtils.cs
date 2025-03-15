c48f79debb90bd48d619a4a560f56a41561a8eac
Mikita Besau <mikitabesau@gmail.com>
2025-03-15 13:10:00 +0200

    modified password hashing to one supported by azure

--- task4/Utils/AccountUtils.cs
+++ task4/Utils/AccountUtils.cs
@@ -7,7 +7,7 @@ namespace task4.Utils
     {
         public static string HashPassword(string password)
         {
-            using var sha256 = SHA3_256.Create();
+            using var sha256 = SHA256.Create();
             var bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
             return Convert.ToBase64String(bytes);
         }
